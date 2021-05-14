674d7214cc90e3c3982706a6d4524f41538a9b5b
JohnOrt31 <jonny.ort31@gmail.com>
2021-05-14 14:12:19 -0500

    Adding Path parameters

--- main.py
+++ main.py
@@ -4,8 +4,19 @@ app  = FastAPI()
 
 @app.get('/')
 def index():
-    return {'data': {'name': 'Sarthak'}}
+    return {'data': 'Blog List'}
 
-@app.get('/about')
-def about():
-    return {'data': 'about page'}
+@app.get('/blog/unpublished')
+def unpublished():
+    return {'data': 'all unpublished blogs'}
+
+@app.get('/blog/{id}')
+def show(id: int):
+    # Fetch blog with id = id
+    return {'data': id}
+
+
+@app.get('/blog/{id}/comments')
+def comment(id):
+    # Fetch comments of blog with id = id
+    return {'data': {'1','2'}}
