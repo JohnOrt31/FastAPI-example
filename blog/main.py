0e3f52d6bf4a8ff40039b7e86855fff835fc437e
JohnOrt31 <jonny.ort31@gmail.com>
2021-05-19 17:21:59 -0500

    Addings status code and adding delete blog

--- blog/main.py
+++ blog/main.py
@@ -1,8 +1,12 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, status, Response, HTTPException
 from fastapi.params import Depends
+from sqlalchemy.sql.functions import mode
+from starlette.status import HTTP_404_NOT_FOUND
 from . import schemas, models
 from .database import engine, SessionLocal
 from sqlalchemy.orm import Session
+
+import blog
 
 app = FastAPI()
 
@@ -15,7 +19,8 @@ def get_db():
     finally: 
         db.close()
 
-@app.post('/blog')
+# Create a blog
+@app.post('/blog', status_code=status.HTTP_201_CREATED)
 def create(request: schemas.Blog, db: Session = Depends(get_db)):
     new_blog = models.Blog(title=request.title, body=request.body)
     db.add(new_blog)
@@ -23,13 +28,27 @@ def create(request: schemas.Blog, db: Session = Depends(get_db)):
     db.refresh(new_blog)
     return new_blog
 
+# Delete a blog
+@app.delete('/blog/{id}', status_code=status.HTTP_204_NO_CONTENT)
+def delete_blog(id, db: Session = Depends(get_db)):
+    db.query(models.Blog).filter(models.Blog.id == id).delete(synchronize_session=False)
+    db.commit()
+    return 'Done'
+
+# Show all blogs
 @app.get('/blog')
 def all(db: Session = Depends(get_db)):
     blogs = db.query(models.Blog).all()
     return blogs
 
-@app.get('/blog/{id}')
-def show(id, db: Session = Depends(get_db)):
+# Show a blog by id
+@app.get('/blog/{id}', status_code=200)
+def show(id, response: Response, db: Session = Depends(get_db)):
     blog = db.query(models.Blog).filter(models.Blog.id == id).first()
+    if not blog:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"Blog with the id {id} is not available")
+        # response.status_code = status.HTTP_404_NOT_FOUND
+        # return {'details': f'Blog with the id {id} is not available'}
     return blog
-    
+
